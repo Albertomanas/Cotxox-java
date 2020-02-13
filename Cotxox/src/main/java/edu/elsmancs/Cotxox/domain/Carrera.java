4a1244a3717eff0d5bb122a30591795787a282e9
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-13 14:37:56 +0100

    feat(Carrera): Inicio
    Creación de GETTER y SETTERS + propiedades y metodo constructor

--- Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/Carrera.java
+++ Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/Carrera.java
@@ -1,4 +1,42 @@
 package edu.elsmancs.Cotxox.domain;
 
 public class Carrera {
+
+    private int tiempoEsperado = 0;
+    private int tiempoCarrera = 0;
+    private double costeTotal = 0; // double --> tipo de dato de 64bits
+    private Conductor[] conductor = null;
+
+    private String tarjetaCredito = null;
+    private String origen = null;
+    private String destino = null;
+    private double distancia = 0;
+
+
+    public Carrera(String tarjetaCredito) {  //Método constructor
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public String getTarjetaCredito() {
+        return this.tarjetaCredito;
+    }
+
+    public String getOrigen() {
+        return this.origen;
+    }
+
+    public String getDestino() {
+        return this.destino;
+    }
+
+    public double getDistancia() {
+        return this.distancia;
+    }
+
+  /*  public Boolean comprobarTarjetaCredito() {
+        if (tarjetaCredito.length() != 12)
+            return false;
+        }
+   */
 }
+
