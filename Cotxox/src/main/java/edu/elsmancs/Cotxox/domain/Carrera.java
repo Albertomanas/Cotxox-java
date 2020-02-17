d89b06ed195e61e9a3cac04d1610d7df51515b5a
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-17 01:18:31 +0100

    feat(Carrera): Añadir lógica en la clase Carrera

--- Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/Carrera.java
+++ Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/Carrera.java
@@ -1,16 +1,19 @@
 package edu.elsmancs.Cotxox.domain;
 
+import edu.elsmancs.Cotxox.domain.Tarifa;
+
 public class Carrera {
 
     private int tiempoEsperado = 0;
     private int tiempoCarrera = 0;
     private double costeTotal = 0; // double --> tipo de dato de 64bits
-    private Conductor[] conductor = null;
+    private Conductor conductor = null;
 
     private String tarjetaCredito = null;
     private String origen = null;
     private String destino = null;
     private double distancia = 0;
+    private int propina = 0;
 
 
     public Carrera(String tarjetaCredito) {  //Método constructor
@@ -25,12 +28,52 @@ public class Carrera {
         return this.origen;
     }
 
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
     public String getDestino() {
         return this.destino;
     }
 
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
     public double getDistancia() {
         return this.distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public int getTiempoCarrera() {
+        return this.tiempoCarrera;
+    }
+
+    public void setTiempoCarrera(int tiempoCarrera){
+        this.tiempoCarrera = tiempoCarrera;
+    }
+
+    public double getCosteTotal() {
+        return this.costeTotal;
+    }
+
+    public int getTiempoEsperado() {
+        return this.tiempoEsperado;
+    }
+
+    public void setTiempoEsperado(int tiempoEsperado) {
+        this.tiempoEsperado = tiempoEsperado;
+    }
+
+    public Conductor getConductor() {
+        return this.conductor;
+    }
+
+    public void setConductor(Conductor conductor) {
+        this.conductor = conductor;
     }
 
   /*  public Boolean comprobarTarjetaCredito() {
@@ -38,5 +81,17 @@ public class Carrera {
             return false;
         }
    */
+
+  //Métodos para operar según lo pedido por el enunciado:
+
+    public double getCosteEsperado() {  //GETTER dependiente de los métodos de tarifa
+        return Tarifa.getCosteTotalEsperado(this);   //Hace referencia a la carrera procesada
+    }
+
+   /* public Conductor asignarConductor(PoolConductores conductores) {
+
+    }
+
+    */
 }
 
