2b099e56a67dce375f3592e9d52411c29bf7e526
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-17 01:20:39 +0100

    feat(Tarifa): Crear clase tarifa con métodos
    Uso de Elvis Operator

---
+++ Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/Tarifa.java
@@ -0,0 +1,27 @@
+package edu.elsmancs.Cotxox.domain;
+
+import edu.elsmancs.Cotxox.domain.Carrera;
+public class Tarifa {
+    final static private double costeMilla = 1.35;  //Porqué final static(?)
+    final static private double costeMinuto = 0.35;
+    final static private double costeMinimo = 5.0;
+    final static byte porcentajeComision = 20;
+
+    public static double getCosteDistancia(double distancia) {  //Distancia de Carrera
+        return distancia * costeMilla;
+    }
+
+    public static double getCosteTiempo(int minuto) {  //Multiplicar minuto por el coste de este
+        return minuto * costeMinuto;
+    }
+
+    public static double getCosteTotalEsperado(Carrera carrera){
+        /*Coste esperado de la carrera
+        * en función de la distancia esperada y el tiempo esperado  (Linea 23)
+        * El costeTotal no puede ser inferior al mínimo (costeTotal>costeMinimo)
+        */
+       double costeTotal = getCosteDistancia(carrera.getDistancia()) + getCosteTiempo(carrera.getTiempoEsperado());
+       return costeTotal >= costeMinimo ? costeTotal : costeMinimo;
+       //operador coalescente = || = :? = Elvis operator
+    }
+}
