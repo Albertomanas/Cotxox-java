5c360e79944608aa8fcfb9f86edb78a3726b0134
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-19 14:13:46 +0100

    feat(PoolConductores): Lógica acabada de PoolConductores
    Alcanzado con dificultad, queda pendiente revisión de Arrays

--- Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/PoolConductores.java
+++ Cotxox/src/main/java/edu/elsmancs/Cotxox/domain/PoolConductores.java
@@ -1,6 +1,7 @@
 package edu.elsmancs.Cotxox.domain;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PoolConductores {
 
@@ -9,8 +10,31 @@ public class PoolConductores {
 
     public PoolConductores(ArrayList<Conductor> poolConductores) {
         this.poolConductores = poolConductores;
-        // Constructor recibe un array de longitud <Conductor>
+        /* Array PoolConductores de Conductores
+        *   Permite operar con el objeto Conductores
+         */
     }
 
+    public ArrayList<Conductor> getPoolConductores() {
+        return this.poolConductores;
+    }
+
+    public Conductor asignarConductor() {
+        Conductor conductor = new Conductor();
+        Random random = new Random();
+        boolean asignado = false;
+        while (!asignado) {  //Sea distinto del valor inicial false
+            int index = random.nextInt(getPoolConductores().size());
+            /* clase random añade al siguiente de la
+             * longitud de la Array PoolConductores
+             */
+            conductor = getPoolConductores().get(index); //devuelve array actualizada
+            if(!conductor.isOcupado()) { //
+                conductor.setOcupado(true); //Conductor con cliente
+                asignado = true;
+            }
+        }
+        return conductor;
+    }
 
 }
